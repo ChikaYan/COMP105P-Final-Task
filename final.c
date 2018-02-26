39775b80e0c3475f91d0a67810f766ff4ea9e7e9
Walter Wu <walterwuyan@gmail.com>
2018-02-26 17:19:27 +0000

    Add constants

---
+++ final.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "simpletools.h"
+#include "abdrive.h"
+#include "simpletools.h"
+#include "ping.h"
+#include "basics.h"
+
+const int SQUARE_LENGTH = 120;
+const int LR_THRESHOLD = 30;
+const int FRONT_THRESHOLD = 18;
+
+int main() {
+
+}
