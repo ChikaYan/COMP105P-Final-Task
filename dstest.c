b7e3e094d6f4f4d38d2f214a9d1b2decbf95b651
Walter Wu <walterwuyan@gmail.com>
2018-03-19 20:55:58 +0000

    Finish dstest

--- dstest.c
+++ dstest.c
@@ -15,18 +15,77 @@
 int left, right;
 int preL, preR;
 
-void turnRightAfterForwarding(){
+void turnRightAfterInit() {
     drive_getTicks(&preL, &preR);
     drive_ramp(128, 64);
-    pause(850);
+    pause(710);
     drive_getTicks(&left, &right);
-    printf("Travelled for (%d,%d)\n", left - preL, right - preR);
+    printf("turnRightAfterInit: Travelled for (%d,%d)\n", left - preL, right - preR);
+    drive_ramp(128, 128);
+    pause(170);
 }
 
-void turnLeftAfterTurningRight(){
+void turnRight() { // tested: after forwarding, after turning left
     drive_getTicks(&preL, &preR);
-    drive_ramp(60, 128);
-    pause(750);
+    drive_ramp(128, 64);
+    pause(785);
+    drive_getTicks(&left, &right);
+    printf("turnRight: Travelled for (%d,%d)\n", left - preL, right - preR);
+    drive_ramp(128, 128);
+    pause(170);
+}
+
+void turnRightAfterRightTurn(){
+    drive_ramp(128, 128);
+    pause(110);
+    drive_ramp(128, 64);
+    pause(625);
+    drive_getTicks(&left, &right);
+    printf("turnRightAfterRightTurn: Travelled for (%d,%d)\n", left - preL, right - preR);
+    drive_ramp(128, 128);
+    pause(170);
+}
+
+void turnLeft() { // tested: after forwarding, after turning right, after turning init right
+    drive_getTicks(&preL, &preR);
+    drive_ramp(64, 128);
+    pause(785);
+    drive_getTicks(&left, &right);
+    printf("turnLeft: Travelled for (%d,%d)\n", left - preL, right - preR);
+    drive_ramp(128, 128);
+    pause(170);
+}
+
+void turnLeftAfterLeftTurn(){
+    drive_ramp(128, 128);
+    pause(110);
+    drive_ramp(64, 128);
+    pause(625);
+    drive_getTicks(&left, &right);
+    printf("turnLeftAfterLeftTurn: Travelled for (%d,%d)\n", left - preL, right - preR);
+    drive_ramp(128, 128);
+    pause(170);
+}
+
+void moveForward() {
+    drive_getTicks(&preL, &preR);
+    drive_ramp(128, 128);
+    pause(940);
+    drive_getTicks(&left, &right);
+    printf("moveForward: Travelled for (%d,%d)\n", left - preL, right - preR);
+}
+
+//void moveForwardAfterTurn() {
+//    drive_getTicks(&preL, &preR);
+//    drive_ramp(128, 128);
+//    pause(1100);
+//    drive_getTicks(&left, &right);
+//    printf("moveForwardAfterTurn: Travelled for (%d,%d)\n", left - preL, right - preR);
+//}
+
+void initMove() {
+    drive_ramp(128, 128);
+    pause(1150);
     drive_getTicks(&left, &right);
     printf("Travelled for (%d,%d)\n", left - preL, right - preR);
 }
@@ -35,24 +94,14 @@ int main() {
     drive_setRampStep(2000);
     simulator_startNewSmokeTrail();
 
-    drive_ramp(128, 128);
-    pause(1150);
-    pause(1000);
-    drive_getTicks(&left, &right);
-    printf("Travelled for (%d,%d)\n", left - preL, right - preR);
+    initMove();
 
-    turnRightAfterForwarding();
+    turnRightAfterInit();
+    turnLeft();
+    moveForward();
+    turnLeft();
+    turnLeftAfterLeftTurn();
 
-//    turnLeftAfterTurningRight();
-
-
-//    drive_getTicks(&preL, &preR);
-//    drive_ramp(128, 0);
-//    pause(750);
-//    drive_getTicks(&left, &right);
-//    printf("Travelled for (%d,%d)\n", left - preL, right - preR);
-//    drive_ramp(128, 128);
-//    pause(150);
 
     drive_ramp(128, 128);
     while (1);
